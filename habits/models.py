a0f31c5717489e74c9faa23ab99e2af5533c2ece
hrtaeu <100742860+hrtaeu@users.noreply.github.com>
2025-03-10 13:19:52 +0800

    Delete habits directory

--- habits/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Habit(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=255)
-    is_completed = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.name
