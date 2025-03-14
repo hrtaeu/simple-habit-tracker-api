fde4dc6063bb846d2f1f655b28d45fbf9afb7f24
hrtaeu <100742860+hrtaeu@users.noreply.github.com>
2025-03-14 23:45:41 +0800

    Update serializers.py

--- habit_tracker/habits/serializers.py
+++ habit_tracker/habits/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Habit
+from .models import HabitTimeLog
+
 
 class HabitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Habit
+        fields = '__all__'
+
+class HabitTimeLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HabitTimeLog
         fields = '__all__'
