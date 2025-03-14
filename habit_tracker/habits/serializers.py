e7eec9efc70ef1f2eacb79163133a23b4402c7e0
hrtaeu <100742860+hrtaeu@users.noreply.github.com>
2025-03-15 00:29:47 +0800

    Update serializers.py

--- habit_tracker/habits/serializers.py
+++ habit_tracker/habits/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Habit
 from .models import HabitTimeLog
+from django.contrib.auth.models import User
 
 
 class HabitSerializer(serializers.ModelSerializer):
@@ -15,3 +16,20 @@ class HabitTimeLogSerializer(serializers.ModelSerializer):
 
 class ResetStreakSerializer(serializers.Serializer):
     habit_id = serializers.IntegerField()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
